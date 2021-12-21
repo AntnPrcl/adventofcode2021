daa29673ac4b9daf1638e71986f081e0c7199f15
Antoine Pourcel <antoine.pourcel@aisi.fr>
2021-12-21 13:07:45 +0100

    Add Day02 part 1

---
+++ Go/day02.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type course struct {
+	way  string
+	dist int
+}
+
+func read_input() (input []course) {
+	b, _ := os.ReadFile("../inputs/day02.txt")
+
+	lines := strings.Split(string(b), "\n")
+
+	//	input := make([]course, 0, len(lines))
+
+	for _, l := range lines {
+
+		if len(l) == 0 {
+			continue
+		}
+
+		e := strings.Split(l, " ")
+		n, _ := strconv.Atoi(e[1])
+
+		input = append(input, course{way: e[0], dist: n})
+	}
+
+	return input
+}
+
+func day02_I() {
+	//test := []course{{way: "forward", dist: 5}, {"down", 5}, {"forward", 8}, {"up", 3}, {"down", 8}, {"forward", 2}}
+	input := read_input()
+	x := 0
+	y := 0
+	for i := 0; i < len(input); i++ {
+		if input[i].way == "forward" {
+			x = x + input[i].dist
+		}
+		if input[i].way == "down" {
+			y += input[i].dist
+		}
+		if input[i].way == "up" {
+			y -= input[i].dist
+		}
+	}
+	println("X : ", x, "\nY : ", y)
+	fmt.Println("Ans : ", x*y)
+}
+
+func day02_II() {
+
+}
+
+func main() {
+	fmt.Println("*** ADVENT OF CODE 2021 ** DAY02 ***")
+	fmt.Println("====== PART I ======")
+	day02_I()
+}
