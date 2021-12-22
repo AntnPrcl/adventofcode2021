290984a5264b463f7c875d679ee6169bc54b4fc5
AntnPrcl <a.pourcel@live.fr>
2021-12-22 15:19:41 +0100

    Add Day 3 part II

--- Go/day03.go
+++ Go/day03.go
@@ -34,6 +34,7 @@ func read_input() (input [][]int) {
 func most_common_byte(input [][]int, index int) int {
 	n1 := 0
 	n0 := 0
+
 	for i := 0; i < len(input); i++ {
 		if input[i][index] == 0 {
 			n0++
@@ -41,7 +42,7 @@ func most_common_byte(input [][]int, index int) int {
 			n1++
 		}
 	}
-	if n1 > n0 {
+	if n1 >= n0 {
 		return 1
 	} else {
 		return 0
@@ -58,11 +59,33 @@ func less_common_byte(input [][]int, index int) int {
 			n1++
 		}
 	}
-	if n1 > n0 {
+	if n1 >= n0 {
 		return 0
 	} else {
 		return 1
 	}
+}
+
+func filter_most_common(input [][]int, index int) [][]int {
+	bit := most_common_byte(input, index)
+	var input_copy [][]int
+	for i := 0; i < len(input); i++ {
+		if input[i][index] == bit {
+			input_copy = append(input_copy, input[i])
+		}
+	}
+	return input_copy
+}
+
+func filter_less_common(input [][]int, index int) [][]int {
+	bit := less_common_byte(input, index)
+	var input_copy [][]int
+	for i := 0; i < len(input); i++ {
+		if input[i][index] == bit {
+			input_copy = append(input_copy, input[i])
+		}
+	}
+	return input_copy
 }
 
 func convert_result(input []int) int {
@@ -99,8 +122,33 @@ func day03_I() {
 	fmt.Println("Ans : ", result)
 }
 
+func day03_II() {
+
+	input := read_input()
+	input_copy := input
+	for i := 0; i < len(input_copy[0]); i++ {
+		input_copy = filter_most_common(input_copy, i)
+		if len(input_copy) == 1 {
+			break
+		}
+	}
+	oxygen_generator_rating := convert_result(input_copy[0])
+	fmt.Println("oxygen generator rating :", oxygen_generator_rating)
+	input_copy = input
+	for i := 0; i < len(input_copy[0]); i++ {
+		input_copy = filter_less_common(input_copy, i)
+		if len(input_copy) == 1 {
+			break
+		}
+	}
+	co2_scrubber_rating := convert_result(input_copy[0])
+	fmt.Println("CO2 scrubber rating :", co2_scrubber_rating)
+	fmt.Println("Answer :", co2_scrubber_rating*oxygen_generator_rating)
+}
 func main() {
 	fmt.Println("*** ADVENT OF CODE 2021 ** DAY 03 ***")
 	fmt.Println("====== PART I ======")
 	day03_I()
+	fmt.Println("====== PART II ======")
+	day03_II()
 }
