867872c18647867a9e7e7795c8694ac9970a56fa
AntnPrcl <a.pourcel@live.fr>
2021-12-27 17:19:11 +0100

    Add Day04 Part I in Golang

--- Go/day04.go
+++ Go/day04.go
@@ -1,7 +1,9 @@
-package day04
+package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,15 +13,75 @@ type cell struct {
 }
 
 func read_input() ([]int, [][][]cell) {
-	input, _ := os.ReadFile("../inputs/day04.txt")
+	input, _ := os.ReadFile("inputs/day04.txt")
 	lines := strings.Split(string(input), "\n")
-	nums := lines[0]
-
+	nums := strings.Split(lines[0], ",")
+	var new_nums []int
+	for _, num := range nums {
+		new_value, _ := strconv.Atoi(num)
+		new_nums = append(new_nums, new_value)
+	}
 	tables := strings.Split(string(input), "\n\n")
 	tables = tables[1:]
-	for table := 0; table < len(tables); table++ {
-		tables[table] := strings.Split(string(tables[table]), "\n")
-
+	var new_tables [][][]cell
+	for _, table := range tables {
+		table_slice := strings.Split(string(table), "\n")
+		var new_table [][]cell
+		for _, row := range table_slice {
+			row = strings.Replace(row, "  ", " ", -1)
+			row_slice := strings.Split(row, " ")
+			if row_slice[0] == "" {
+				row_slice = row_slice[1:]
+			}
+			var new_row []cell
+			for _, column := range row_slice {
+				var new_column cell
+				new_column.draw = false
+				new_column.number, _ = strconv.Atoi(column)
+				new_row = append(new_row, new_column)
+			}
+			new_table = append(new_table, new_row)
+		}
+		new_tables = append(new_tables, new_table)
 	}
-	return nums, tables
+
+	return new_nums, new_tables
+}
+
+func day04_I(tables [][][]cell, nums []int) {
+	for _, num := range nums {
+		for i, table := range tables {
+			for j, row := range table {
+				for k, column := range row {
+					if column.number == num {
+						tables[i][j][k].draw = true
+					}
+				}
+			}
+		}
+
+		for _, table := range tables {
+			for i := 0; i < 5; i++ {
+				if (table[i][0].draw == true && table[i][1].draw == true && table[i][2].draw == true && table[i][3].draw == true && table[i][4].draw == true) || (table[0][i].draw == true && table[1][i].draw == true && table[2][i].draw == true && table[3][i].draw == true && table[4][i].draw == true) {
+					score := 0
+					for _, row := range table {
+						for _, column := range row {
+							if column.draw == false {
+								score += column.number
+							}
+						}
+					}
+					score = score * num
+					fmt.Println("Ans :", score)
+					return
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	read_input()
+	nums, tables := read_input()
+	day04_I(tables, nums)
 }
